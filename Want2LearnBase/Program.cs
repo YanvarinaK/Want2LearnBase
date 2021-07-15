e277d17d7720be3e1ed394a1b52f2decdb0f1557
Яна <yankayanka4815@gmail.com>
2021-07-15 18:10:04 +0300

    create function

--- Want2LearnBase/Program.cs
+++ Want2LearnBase/Program.cs
@@ -10,7 +10,8 @@ namespace Want2LearnBase
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(GetCountContains("Артем пидр пидрило пидрилка", "пидр"));
+            Console.ReadKey();
         }
         public static int[] RandomArray(int number, Random rnd)
         {
@@ -45,6 +46,104 @@ namespace Want2LearnBase
                 }
             }
            
+        }
+        public static void Swap(ref string line1, ref string line2)
+        {
+            string Temp;
+            Temp = line1;
+            line1 = line2;
+            line2 = Temp;
+        }
+        public static void Swap(ref int first, ref int second)
+        {
+            int Temp;
+            Temp = first;
+            first = second;
+            second = Temp;
+        }
+        public static void Swap(ref double first, ref double second)
+        {
+            double Temp;
+            Temp = first;
+            first = second;
+            second = Temp;
+        }
+        public static void Swap(ref decimal first, ref decimal second)
+        {
+            decimal Temp;
+            Temp = first;
+            first = second;
+            second = Temp;
+        }
+        public static void Swap(ref byte first, ref byte second)
+        {
+            byte Temp;
+            Temp = first;
+            first = second;
+            second = Temp;
+        }
+        public static void Swap(ref float first, ref float second)
+        {
+            float Temp;
+            Temp = first;
+            first = second;
+            second = Temp;
+        }
+        public static string GetAddress(string city, string street, string houseNumber, string flatNumber)
+        {
+            string FullAdress;
+            FullAdress = String.Format("г. {0}, ул. {1}, д. {2}, кв. {3}", city, street, houseNumber, flatNumber);
+            return FullAdress;
+        }
+        private static void Sort(ref int firstNumber, ref int secondNumber)
+        {
+            if (firstNumber < secondNumber)
+            {
+                Swap(ref firstNumber, ref secondNumber);
+            }
+        }
+        public static int FindNOD(int firstNumber, int secondNumber)
+        {
+            Sort(ref firstNumber, ref secondNumber);
+           
+            int result;
+            int ost;
+            do
+            {
+                if (secondNumber == 0)
+                    break;
+                ost = firstNumber % secondNumber;
+                firstNumber = secondNumber;
+                secondNumber = ost;
+            }
+            while (ost != 0);
+            result = firstNumber;
+            return result;
+        }
+        public static int GetCountContains(string line, char symbol)
+        {
+            int quantity = 0;
+            if (line == null)
+            {
+                return 0;
+            }
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == symbol)
+                {
+                    quantity++;
+                }
+            }
+
+            return quantity;
+        }
+        public static int GetCountContains(string line, string word)
+        {
+            int quantity;
+            string newLine = line.Replace(word, "");
+            quantity = (line.Length - newLine.Length) / word.Length;
+            return quantity;
+
         }
     }
 }
